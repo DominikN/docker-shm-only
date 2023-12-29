dbb41f9d054f968920ea3f3c275c2cbc37f51a59
dominikn <dominik.nwk@gmail.com>
2023-12-29 10:25:41 +0100

    better entrypoint

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-# Create a new user (set with compose.yaml -> user: ${USER_ID})
-useradd -ms /bin/bash $USER
-
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 
+if [ -z "$USER" ]; then
+    export USER=root
+elif ! id "$USER" &>/dev/null; then
+    useradd -ms /bin/bash "$USER"
+fi
 exec gosu $USER "$@"
